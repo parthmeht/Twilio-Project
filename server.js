53c2db1e7fb289a405e55ce35a6bf558040c0664
Parth Mehta <parthmeht@gmail.com>
2019-11-19 19:06:42 -0500

    code update

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-var http = require('http'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    MessagingResponse = require('twilio').twiml.MessagingResponse;
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
 
+const app = express();
 
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', function(req, res) {
-    var twiml = new MessagingResponse();
-    console.log(req.body.Body);
-    if (req.body.Body == 'hello') {
-        twiml.message('Hi!');
-    } else if(req.body.Body == 'bye') {
-        twiml.message('Goodbye');
-    } else {
-        twiml.message('No Body param match, Twilio sends this in the request to your server.');
-    }
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
+app.post('/', (req, res) => {
+  const twiml = new MessagingResponse();
+  console.log(req.body.Body);
+  if (req.body.Body == 'hello') {
+    twiml.message('Hi!');
+  } else if (req.body.Body == 'bye') {
+    twiml.message('Goodbye');
+  } else {
+    twiml.message(
+      'No Body param match, Twilio sends this in the request to your server.'
+    );
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, function () {
-    console.log("Express server listening on port 1337");
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
 });
