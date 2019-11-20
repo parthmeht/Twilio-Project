5bf3afca8d255c3b04c3816f705863b0876eb4a1
Parth Mehta <parthmeht@gmail.com>
2019-11-19 19:04:26 -0500

    api update

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+var http = require('http'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const app = express();
 
-app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-  twiml.message('The Robots are coming! Head for the hills!');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+app.post('/', function(req, res) {
+    var twiml = new MessagingResponse();
+    console.log(req.body.Body);
+    if (req.body.Body == 'hello') {
+        twiml.message('Hi!');
+    } else if(req.body.Body == 'bye') {
+        twiml.message('Goodbye');
+    } else {
+        twiml.message('No Body param match, Twilio sends this in the request to your server.');
+    }
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
+http.createServer(app).listen(1337, function () {
+    console.log("Express server listening on port 1337");
 });
